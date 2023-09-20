9e665efb68121743c62b8365d33115fc0ac8cd0d
Alexander Weinrauch <alexander.weinrauch@tugraz.at>
2023-09-20 15:59:04 +0200

    Better search with cmp

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -6,6 +6,19 @@ return {
       mapping= {
        ["<C-e>"] = cmp.mapping.confirm { select = true },
       }
-    }
+    },
+    config = function(plugin, opts)
+      local cmp = require "cmp"
+      -- run cmp setup
+      cmp.setup(opts)
+
+      -- configure `cmp-cmdline` as described in their repo: https://github.com/hrsh7th/cmp-cmdline#setup
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "buffer" },
+        },
+      })
+    end
   }
 }
